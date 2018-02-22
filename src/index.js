63271b426000acdbf0fc7c2e50abb4732934aa5d
Coder <31937370+Code1441@users.noreply.github.com>
2018-02-22 23:52:00 +0300

    code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var a, c, b;
+   var array = equation.split('x');
+    a = +array[0].slice(0,-3);
+
+    if(array[1][3]=='-') b = 0-array[1].slice(5,-3);
+    else  b = +array[1].slice(5,-3);
+
+   if(array[2][1]=='-') c = 0-array[2].slice(3);
+   else  c = +array[2].slice(3);
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
+   var D = Math.round(Math.sqrt(b*b-4*a*c));
+   console.log(D)
+   var result = [];
+
+   var x1 = (-b+D)/(2*a);
+    var x2 = (-b-D)/(2*a);
+
+   result.push(x1,x2);
+   result.sort(a,b=>a-b);
+   return result;
 }
